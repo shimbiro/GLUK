887edeb32da1cf0c0ee06fa314e54b2948d677cc
shimbiro <shimbirosharon@gmail.com>
2016-11-12 18:52:16 +0300

    users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,26 +16,4 @@ class UsersController < ApplicationController
 
 	end
 
-	def create
-		@user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.json { head :no_content }
-    end
-  end
-def user_params
-      params.require(:user).permit(:name, :email)
-  end
-end
+	
